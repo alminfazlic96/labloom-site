9fc4299cd7b09251002e4afda7a38c2559edef93
alminfazlic96 <alminfazlic96@gmail.com>
2026-05-30 01:35:15 +0200

    fixed menu layout

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ export default function LaBloomCaffeTemplate() {
           Meni
         </h2>
 
-        <div  className="max-w-6xl mx-auto"></div>>
+      <div className="max-w-6xl mx-auto"></div>
           {/* LEFT */}
           <div className="bg-zinc-900 p-10 rounded-3xl shadow-xl">
             <h3 className="text-3xl font-bold mb-8 text-yellow-400">
